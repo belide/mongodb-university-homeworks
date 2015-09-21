27177020b8ee1b0841e746583e255c7ef095a219
Maxim Abramchuk <maximabramchuck@gmail.com>
2015-09-21 15:13:49 +0300

    Add solution for 2 task from final exam

---
+++ final_exam/question2.js
@@ -0,0 +1,24 @@
+use enron;
+
+db.messages.aggregate([
+  {$unwind: '$headers.To'},
+  {$group:
+   {_id: {'_id': '$_id', From: '$headers.From'},
+    To: {
+      $addToSet: '$headers.To'
+    }
+   }
+  },
+  {$unwind: '$To'},
+  {$group:
+   {_id: {From: '$_id.From', To:'$To'},
+    count: {
+      $sum: 1
+    }
+   }
+  },
+  {$sort:
+    {'count': -1}
+  },
+  {'$limit': 1}
+]);
